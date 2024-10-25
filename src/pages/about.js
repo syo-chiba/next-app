436241c6b67bb31013e79fe6bb18aae9aef11dd0
s.chiba <s.chiba@ebacorp.jp>
2024-10-25 15:16:05 +0900

    ファイルベースのルーティング方法

---
+++ src/pages/about.js
@@ -0,0 +1,4 @@
+// pages/about.js
+export default function About() {
+  return <h1>私たちについてのページです</h1>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
   return <h1>ホームページです</h1>;
 }
